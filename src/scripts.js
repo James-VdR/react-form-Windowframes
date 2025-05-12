580c0aa96d901522d506758f37ffd2881cf2d325
James-VdR <jamie.v.d.roest@Gmail.com>
2025-05-12 15:56:53 +0200

    Additions
    Added Rotating cube/camera orbit controls for placeholders.

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 export function initThree(container) {
   const width = container.clientWidth;
@@ -9,15 +10,39 @@ export function initThree(container) {
   container.appendChild(renderer.domElement);
 
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-  camera.position.z = 5;
 
-  const axesHelper = new THREE.AxesHelper(5);
-  scene.add(axesHelper);
+  //camera
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    width / 
+    height, 
+    0.1, 
+    1000);
 
-  const animate = () => {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-  };
-  animate();
+const orbit = new OrbitControls(camera, renderer.domElement)
+camera.position.set (0,2,5);
+orbit.update();
+  //camera
+
+  //Geometry
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00})
+const box = new THREE.Mesh(boxGeometry,boxMaterial);
+scene.add(box);
+  //Geometry
+
+    //axes line
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+    //axes line
+
+    //Rotation
+function animate(){
+        box.rotation.x += 0.01;
+        box.rotation.y += 0.01;
+        renderer.render(scene,camera);
+
+    }
+    //Rotation
+renderer.setAnimationLoop(animate);
 }
